08241cf85bd45ebc5537e8cf22ba55e543414d6a
Joy Barman <barmanjoy26@yahoo.com>
2024-06-29 19:04:38 +0600

    Feature for real time update on notification model changes

--- config/notifications/consumers.py
+++ config/notifications/consumers.py
@@ -3,10 +3,12 @@ import logging
 
 from django.contrib.auth import get_user_model
 
-from rest_framework_simplejwt.tokens import AccessToken
-
-from notifications.models import Notification
-from notifications.serializers import UserNotificationListWithCountSerializer
+from notifications.utils import (
+    get_serialized_notifications,
+    get_user,
+    validate_token,
+    get_group_name,
+)
 
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
@@ -23,7 +25,7 @@ class NotificationConsumer(AsyncWebsocketConsumer):
         token = self.scope["url_route"]["kwargs"].get("access_token")
 
         if token:
-            user_id = self.validate_token(token)
+            user_id = validate_token(token=token)
             if not user_id:
                 await self.close()
                 return
@@ -32,43 +34,31 @@ class NotificationConsumer(AsyncWebsocketConsumer):
             await self.close()
             return
 
-        user = await self.get_user(user_id)
+        user = await get_user(user_id)
 
         if user:
             self.scope["user"] = user
             self.scope["token"] = token
+
+            # Add user to group
+            self.group_name = get_group_name(user=user)
+            await self.channel_layer.group_add(
+                self.group_name,
+                self.channel_name,
+            )
+
+            # Accept connection
             await self.accept()
+
         else:
             # User not found
             await self.close()
 
-    def validate_token(self, token):
-        try:
-            access_token = AccessToken(token)
-            user_id = access_token.payload["user_id"]
-            return user_id
-        except Exception as e:
-            logger.error(f"{e}")
-            return None
-
-    @database_sync_to_async
-    def get_user(self, user_id):
-        try:
-            return User.objects.get(id=user_id)
-        except User.DoesNotExist:
-            logger.error(f"User not found")
-            return None
-
-    @database_sync_to_async
-    def get_notifications(self, user):
-        notifications = Notification().get_current_user_notifications(user=user)
-        serialized_data = UserNotificationListWithCountSerializer(notifications).data
-        return serialized_data
-
     async def receive(self, text_data=None):
         user = self.scope.get("user")
-        token_valid = self.validate_token(self.scope.get("token"))
+        token_valid = validate_token(token=self.scope.get("token"))
 
+        # Return error message if token or user is invalid
         if not token_valid:
             await self.send(
                 text_data=json.dumps({"error": "Token may be invalid or expired"})
@@ -79,9 +69,25 @@ class NotificationConsumer(AsyncWebsocketConsumer):
             await self.send(text_data=json.dumps({"error": "User not authenticated"}))
             return
 
-        notifications = await self.get_notifications(user=user)
+        notifications = await database_sync_to_async(get_serialized_notifications)(
+            user=user
+        )
         await self.send(text_data=json.dumps(notifications))
 
     async def disconnect(self, close_code):
-        logger.warning(f"disconnected {close_code}")
+        # Remove user from the group
+        if self.scope.get("user"):
+            await self.channel_layer.group_discard(
+                self.group_name,
+                self.channel_name,
+            )
+
         self.close()
+        logger.warning(f"disconnected {close_code}")
+
+    async def notification_update(self, event):
+        # Update the user's notifications when any change occurs in the Notification model
+        user = self.scope.get("user")
+        if user:
+            notifications = event["user_notifications"]
+            await self.send(text_data=json.dumps(notifications))
